a2c97d118ad95825996f914b7c7877af4f35011a
mhmadsser22-dotcom <mhmadsser22@gmail.com>
2026-09-17 04:34:03 +0300

    Initialize bot.js with web server and Aternos client

---
+++ bot.js
@@ -0,0 +1,31 @@
+const bedrock = require('bedrock-protocol');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot is running 24/7!');
+});
+
+app.listen(3000, () => {
+  console.log('Web server ready.');
+});
+
+const client = bedrock.createClient({
+  host: 'mhmadsser2.aternos.me',
+  port: 12492,
+  username: 'AFK_Bot',
+  offline: true,
+  version: '1.26.50'
+});
+
+client.on('join', () => {
+  console.log('Bot has joined the Aternos server successfully!');
+});
+
+client.on('error', (err) => {
+  console.log('Error:', err);
+});
+
+client.on('close', () => {
+  console.log('Connection closed.');
+});
